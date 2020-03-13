b6eda015c83154b6b1b655fd02086dca5f2662c3
Luis Fernando Murara <luis.murara@resultadosdigitais.com.br>
2020-03-13 17:10:07 -0300

    Initializes on webNavigation completed

--- javascript/background.js
+++ javascript/background.js
@@ -1,14 +1,20 @@
+function updateWip(tabId) {
+    chrome.tabs.sendMessage(tabId, { action: "update" });
+}
+
 (function () {
-    const networkFilters = {
+    chrome.webNavigation.onCompleted.addListener((details) => {
+        if (details.url.startsWith("https://trello.com/b/")) updateWip(details.tabId);
+    });
+
+    chrome.webRequest.onCompleted.addListener((details) => {
+        updateWip(details.tabId);
+    }, {
         urls: [
             "https://trello.com/*/lists/*",
             "https://trello.com/*/lists",
             "https://trello.com/*/cards/*",
             "https://trello.com/*/cards"
         ]
-    };
-
-    chrome.webRequest.onCompleted.addListener((details) => {
-        chrome.tabs.sendMessage(details.tabId, { action: "update" });
-    }, networkFilters);
+    });
 }());

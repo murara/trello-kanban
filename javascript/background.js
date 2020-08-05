162215eb230e0938594fbac651666d30ddc67d37
Luis Fernando Murara <luis.murara@resultadosdigitais.com.br>
2020-08-05 08:43:53 -0300

    fix: :bug: early loading

--- javascript/background.js
+++ javascript/background.js
@@ -1,10 +1,14 @@
+function startWip(tabId) {
+    chrome.tabs.sendMessage(tabId, { action: "start" });
+}
+
 function updateWip(tabId) {
     chrome.tabs.sendMessage(tabId, { action: "update" });
 }
 
 (function () {
     chrome.webNavigation.onCompleted.addListener((details) => {
-        if (details.url.startsWith("https://trello.com/b/")) updateWip(details.tabId);
+        if (details.url.startsWith("https://trello.com/b/")) startWip(details.tabId);
     });
 
     chrome.webRequest.onCompleted.addListener((details) => {
@@ -13,8 +17,7 @@ function updateWip(tabId) {
         urls: [
             "https://trello.com/*/lists/*",
             "https://trello.com/*/lists",
-            "https://trello.com/*/cards/*",
-            "https://trello.com/*/cards"
+            "https://trello.com/*/card/*"
         ]
     });
 }());
